1cecdf3175361f1858b7f5591de96193e910e911
bilalrehan1504-cpu <bilalrehan1504@gmail.com>
2026-08-28 23:02:57 +0500

    task.s

---
+++ task3.s
@@ -0,0 +1,13 @@
+addi x1, x0, 5
+addi x2, x0, 0  
+addi x1, x2, 32
+add x3, x1, x2
+addi x11, x0, 5
+sub x5, x3, x11
+sub x6, x1, x5
+sub x7, x2, x1
+add x8, x6, x7
+add x9, x8, x5
+add x10, x5, x9
+add x9, x10, x3
+add x0,x0,x0
